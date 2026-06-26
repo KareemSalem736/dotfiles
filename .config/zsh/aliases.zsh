9ab4095b7cda2ffa47486a9a5879162906f73745
Kareem Salem <KareemSalem736@gmail.com>
2026-06-26 14:32:03 -0400

    vault backup: 2026-06-26 14:32:03 - M .config/zsh/aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -78,7 +78,7 @@ alias air='$(go env GOPATH)/bin/air'
 # git
 # -------------------------------------------------------------------
 alias gca='git commit -a'
-alias omsb='cd ~/Projects/oms-bridge/'
+alias core='cd ~/Projects/atoms-core/'
 alias s='git switch'
 alias sc='git switch -c'
 alias grp='git fetch --prune'
